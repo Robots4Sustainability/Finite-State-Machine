d673d5801c0f1d5287ac8da09e5e6f5d5c9fd9ec
ashleshp <62256394+ashleshp@users.noreply.github.com>
2025-10-25 16:40:32 +0200

    Delete build directory

--- build/pick_place_fsm/CMakeFiles/mock_servers_only.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for mock_servers_only.
-# This may be replaced when dependencies are built.
--- build/pick_place_fsm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashlesh/r4s/src/Finite-State-Machine
-# Build directory: /home/ashlesh/r4s/src/Finite-State-Machine/build/pick_place_fsm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
